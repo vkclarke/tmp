f67228a2d8c42983c8b311fb374d53359599c0a3
Vincent Clarke <vincent@windows97.space>
2024-01-27 22:12:45 -0500

    pipemaxxing

--- go/json/json.go
+++ go/json/json.go
@@ -3,59 +3,84 @@
 package main
 
 import (
-	"bytes"
+	"io"
 	"log"
 	"os"
-	"strings"
 	"unicode"
+	"unicode/utf8"
 )
 
 func main() {
 	args := os.Args[1:]
 	stderr := log.New(os.Stderr, "[errors] ", 0)
 
-	input := bytes.NewBuffer(make([]byte, 0, os.Getpagesize()))
-	input.ReadFrom(func() *os.File {
+	txt := func() string {
 		if len(args) < 1 {
-			return os.Stdin
-		}
-		f, err := os.Open(args[0])
-		if err != nil {
-			stderr.Fatalln(err)
-		}
-		return f
-	}())
-
-	os.Stdout.WriteString(shrinkwrap(input.String()))
-}
-
-func shrinkwrap(src string) string {
-	var wrapped strings.Builder
-	var quote rune
-	for _, char := range src {
-		switch {
-		case isSpace(char) && quote == 0:
-			continue
-		case char == '"':
-			switch {
-			case quote == char:
-				quote = 0
-			case quote == 0:
-				quote = char
+			input, err := io.ReadAll(os.Stdin)
+			if err != nil {
+				stderr.Fatal(err)
 			}
+			return string(input)
 		}
-		wrapped.WriteRune(char)
+		contents, err := os.ReadFile(args[0])
+		if err != nil {
+			stderr.Fatal(err)
+		}
+		return string(contents)
+	}()
+
+	wait := make(chan struct{})
+	stdout := func() chan<- rune {
+		stdout := make(chan rune)
+		go func() {
+			buf := make([]byte, 4)
+			for char := range stdout {
+				n := utf8.EncodeRune(buf, char)
+				os.Stdout.Write(buf[:n])
+			}
+			close(wait)
+		}()
+		return stdout
+	}()
+
+	for char := range shrinkwrap(iterString(txt)) {
+		stdout <- char
 	}
-	return wrapped.String()
+
+	close(stdout)
+	<-wait
 }
 
-func iterString(txt string, size int) <-chan rune {
-	chars := make(chan rune, size)
+func shrinkwrap(in <-chan rune) <-chan rune {
+	out := make(chan rune)
 	go func() {
-		defer close(chars)
+		var quote rune
+		for char := range in {
+			switch {
+			case isSpace(char) && quote == 0:
+				continue
+			case char == '"':
+				switch {
+				case quote == char:
+					quote = 0
+				case quote == 0:
+					quote = char
+				}
+			}
+			out <- char
+		}
+		close(out)
+	}()
+	return out
+}
+
+func iterString(txt string) <-chan rune {
+	chars := make(chan rune)
+	go func() {
 		for _, char := range txt {
 			chars <- char
 		}
+		close(chars)
 	}()
 	return chars
 }
