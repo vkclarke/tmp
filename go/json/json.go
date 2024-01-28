cefd0c3044782de6eee3e879dbe3b4eeb0924357
Vincent Clarke <vincent@windows97.space>
2024-01-28 03:07:04 -0500

    more cool shit basically

--- go/json/json.go
+++ go/json/json.go
@@ -3,51 +3,74 @@
 package main
 
 import (
-	"io"
+	"bufio"
 	"log"
 	"os"
+	"path/filepath"
 	"unicode"
 	"unicode/utf8"
 )
 
 func main() {
+	name := filepath.Base(os.Args[0])
 	args := os.Args[1:]
-	stderr := log.New(os.Stderr, "[errors] ", 0)
+	errlog := log.New(os.Stderr, name+": ", 0)
 
-	txt := func() string {
-		if len(args) < 1 {
-			input, err := io.ReadAll(os.Stdin)
-			if err != nil {
-				stderr.Fatal(err)
+	if len(args) < 1 {
+		errlog.Fatal("no arguments")
+	}
+
+	var cmd func(<-chan rune) <-chan rune
+	switch args[0] {
+	case "shrink":
+		cmd = shrinkwrap
+	case "expand":
+		cmd = expand
+	default:
+		errlog.Fatalf("invalid command: %q", args[0])
+	}
+
+	in := func() <-chan rune {
+		in := make(chan rune)
+		go func() {
+			b := func() *bufio.Reader {
+				if len(args) < 2 {
+					return bufio.NewReader(os.Stdin)
+				}
+				file, _ := os.Open(args[1])
+				return bufio.NewReader(file)
+			}()
+			for {
+				char, _, err := b.ReadRune()
+				if err != nil {
+					break
+				}
+				in <- char
 			}
-			return string(input)
-		}
-		contents, err := os.ReadFile(args[0])
-		if err != nil {
-			stderr.Fatal(err)
-		}
-		return string(contents)
+			close(in)
+		}()
+		return in
 	}()
 
 	wait := make(chan struct{})
-	stdout := func() chan<- rune {
-		stdout := make(chan rune)
+	out := func() chan<- rune {
+		out := make(chan rune)
 		go func() {
 			buf := make([]byte, 4)
-			for char := range stdout {
+			for char := range out {
 				n := utf8.EncodeRune(buf, char)
 				os.Stdout.Write(buf[:n])
 			}
 			close(wait)
 		}()
-		return stdout
+		return out
 	}()
 
-	for char := range shrinkwrap(iterString(txt)) {
-		stdout <- char
+	// Main loop
+	for char := range cmd(in) {
+		out <- char
 	}
-
-	close(stdout)
+	close(out)
 	<-wait
 }
 
@@ -74,6 +97,47 @@ func shrinkwrap(in <-chan rune) <-chan rune {
 	return out
 }
 
+func expand(in <-chan rune) <-chan rune {
+	out := make(chan rune)
+	go func() {
+		var tabs int
+		indent := func() {
+			for i := 0; i < tabs; i++ {
+				out <- '\t'
+			}
+		}
+		for {
+			char := <-in
+			switch char {
+			case 0:
+				out <- '\n'
+				close(out)
+				return
+			case '{', '[':
+				out <- char
+				out <- '\n'
+				tabs += 1
+				indent()
+			case '}', ']':
+				tabs -= 1
+				out <- '\n'
+				indent()
+				out <- char
+			case ',':
+				out <- char
+				out <- '\n'
+				indent()
+			case ':':
+				out <- char
+				out <- ' '
+			default:
+				out <- char
+			}
+		}
+	}()
+	return out
+}
+
 func iterString(txt string) <-chan rune {
 	chars := make(chan rune)
 	go func() {
@@ -88,13 +152,3 @@ func iterString(txt string) <-chan rune {
 func isSpace(char rune) bool  { return unicode.IsSpace(char) }
 func isNumber(char rune) bool { return unicode.IsNumber(char) }
 func isLetter(char rune) bool { return unicode.IsLetter(char) }
-
-var bounds = map[rune]rune{
-	'(':  ')',
-	'[':  ']',
-	'{':  '}',
-	'<':  '>',
-	'\'': '\'',
-	'"':  '"',
-	'`':  '`',
-}
