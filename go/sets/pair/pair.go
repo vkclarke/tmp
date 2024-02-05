14babc11b5e209972a6b791e860f4b7bff6fa562
Vincent Clarke <vincent@windows97.space>
2024-02-04 19:38:31 -0500

    ok

---
+++ go/sets/pair/pair.go
@@ -0,0 +1,22 @@
+package pair
+
+type Interface[A, B any] interface {
+	A() A
+	B() B
+}
+
+type impl[A, B any] struct {
+	a A
+	b B
+}
+
+func (self impl[A, B]) A() A { return self.A }
+func (self impl[A, B]) B() B { return self.B }
+
+func Of[A, B any](a A, b B) Interface[A, B] {
+	return impl[A, B]{a, b}
+}
+
+func OfSame[T any](a, b T) Interface[T, T] {
+	return impl[T, T]{a, b}
+}
